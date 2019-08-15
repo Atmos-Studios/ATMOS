5b2e6fd06578cde3ff6f3d80e08ffab917ceb995
Austin Foster <52180909+YourDeadPixels@users.noreply.github.com>
2019-08-15 18:54:39 -0500

    ALPHA BASE
    The NEXT generation

---
+++ src/index.js
@@ -0,0 +1,9 @@
+//*Dependencies
+//Discord.js
+const discord = require('discord.js')
+
+//*Loaders
+//Command Loader Script ./src/utils/commandloader.js
+const commandloader = require('./utils/commandloader.js')
+//Events Loader Script ./src/utils/eventloader.js
+const eventloader = require('./utils/eventloader.js')
